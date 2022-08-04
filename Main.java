c90e06dc4abc1c00664593139a2a56a4f5343edc
Shamaniyak <shamaniyak@ya.ru>
2022-08-05 01:18:26 +0300

    Рефакторинг. Создан класс продукта. В этот класс перенесены переменные и добавлена функция getSum.

--- Main.java
+++ Main.java
@@ -5,26 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String products[] = {"Хлеб", "Яблоки", "Молоко", "Морковь", "Конфеты"};
-        int prices[] = {60, 130, 80, 20, 300};
+        Product[] products = {
+                new Product("Хлеб", 60, false),
+                new Product("Яблоки", 130, true),
+                new Product("Молоко", 80, false),
+                new Product("Морковь", 20, true),
+                new Product("Конфеты", 300, false)
+        };
 
-        int sales[] = {0, 1, 0, 1, 0}; // 1 - товар по акции, 0 - без акции
-        String message[] = new String[5];
-        System.out.println("Список возможных товаров для покупки: ");
+        printProducts(products);
 
-        for (int i = 0; i < products.length; i++) {
-            if (sales[i] == 0) {
-                message[i] = " ";
-            } else {
-                message[i] = "по акции \"3 + 2\"";
-            }
-            System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб/шт "
-                    + message[i]);
-        }
-
-        int sumProducts = 0; // общая сумма покупки
-        int count[] = new int[5]; // массив для хранения количества продуктов
-        int sale[] = new int[5];
         while (true) {
             System.out.println("Выберите товар и количество или введите end");
             String input = scanner.nextLine();
@@ -42,79 +32,55 @@ public class Main {
                         System.out.println("Введены нечисловые данные!");
                     }
 
-                    if (productNumber < 0 | productNumber > products.length - 1) {
+                    if (productNumber < 0 || productNumber > products.length - 1) {
                         System.out.println("Введен некорректный номер продукта");
                         continue;
                     }
-                    int currentPrice = prices[productNumber]; // цена продукта по номеру
-                    int sum; // сумма покупки
-                    int saleCount;
 
+                    // если ввели ноль, то убираем продукт из корзины, иначе добавляем количество продукта
                     if (productCount == 0) {
-                        sumProducts = sumProducts - count[productNumber] * currentPrice;
-                        count[productNumber] = 0;
-                    } else if (productCount < 0) {
-                        if (count[productNumber] == 0) {
-                            continue;
-                        } else {
-                            ///int ostatok = count[productNumber] % 3; ///////////
-                            count[productNumber] += productCount;
-                            if (count[productNumber] < 0) {
-                                count[productNumber] = 0;
-                                //sumProducts = 0;
-                                continue;
-                            }
-
-                            if (sales[productNumber] == 1) {
-
-                                saleCount = count[productNumber] / 3; ///////
-                                sale[productNumber] = saleCount; /////
-                                ///sum = (productCount - saleCount) * currentPrice; //сумма покупки //////
-                                ///sumProducts += sum;
-                            } else {
-                                saleCount = 0;
-                                sale[productNumber] = saleCount;
-                                sum = (productCount * currentPrice); // сумма покупки
-                                sumProducts += sum;
-                            }
-                        }
+                        products[productNumber].setCount(0);
                     } else {
-                        int ostatok = count[productNumber] % 3; //////////
-                        count[productNumber] += productCount;
-                        if (sales[productNumber] == 1) {
-                            saleCount = count[productNumber] / 3; ///////
-                            sale[productNumber] = saleCount;//////
-                            sum = (productCount - saleCount) * currentPrice; //сумма покупки ////
-                            sumProducts += sum;
-                        } else {
-                            saleCount = 0;
-                            sale[productNumber] = saleCount;
-                            sum = (productCount * currentPrice); // сумма покупки
-                            sumProducts += sum;
-                        }
+                        products[productNumber].add(productCount);
                     }
                 } else {
                     System.out.println("Введены одна или более двух частей!");
                 }
             }
         }
+
+        printBasket(products);
+    }
+
+    private static void printBasket(Product[] products) {
         System.out.println("Ваша корзина: ");
 
-        sumProducts = 0;
+        double sumProducts = 0; // общая сумма покупки
+
+        // считаем общую сумму и печатаем количество и сумму каждого товара
         for (int i = 0; i < products.length; i++) {
-            if (count[i] > 0) {
-                int sum = (count[i] - sale[i]) * prices[i];
+            if (products[i].getCount() > 0) {
+                double sum = products[i].getSum();
                 sumProducts += sum;
+                System.out.println(products[i].getName() + " " + products[i].getCount() + " шт. "
+                        + products[i].getPrice() + "руб./шт. " + sum + " руб. в сумме");
             }
         }
-
-        for (int i = 0; i < products.length; i++) {
-            if (count[i] > 0) {
-
-                System.out.println(products[i] + " " + count[i] + " шт. "
-                        + prices[i] + "руб./шт. " + (count[i] - sale[i]) * prices[i] + " руб. в сумме");
-            }
-        }
+        // печатаем общую сумму
         System.out.println("Итого: " + sumProducts + " руб.");
+    }
+
+    private static void printProducts(Product[] products) {
+        System.out.println("Список возможных товаров для покупки: ");
+        for (int i = 0; i < products.length; i++) {
+            String message;
+            if (!products[i].isPromo()) {
+                message = " ";
+            } else {
+                message = "по акции \"3 + 2\"";
+            }
+            System.out.println((i + 1) + ". " + products[i].getName() + " " + products[i].getPrice() + " руб/шт "
+                    + message);
+        }
     }
 }
---
+++ Product.java
@@ -0,0 +1,56 @@
+public class Product {
+    private String name;
+    private double price = 0;
+    // true - товар по акции, false - без акции
+    private boolean promo = false;
+    private int count = 0;
+
+    public Product(String name, double price, boolean promo) {
+        this.name = name;
+        this.price = price;
+        this.promo = promo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isPromo() {
+        return promo;
+    }
+
+    public void setPromo(boolean promo) {
+        this.promo = promo;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void add(int count) {
+        this.count += count;
+        if(this.count < 0)
+            this.count = 0;
+    }
+
+    public double getSum() {
+        int sale = count / 3;
+        return (count - sale) * price;
+    }
+}
